4ca6f51b5e8c6f8b0e7f52bf5e2893adf1b9cfed
name <email@email.com>
2014-01-12 12:39:55 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedB::Application.config.secret_key_base = '063825918daa663d81fd03019c9103eff213149721807d70b167b0b27d6c8a6c22c79604a396e98704254663ebe87e015b0ff241d1171366329f6309ce90cb74'
